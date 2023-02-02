e6cb330bc527780aec243d4eef1c495cd06a4724
Luke Graham <luke.evan.graham@gmail.com>
2023-02-02 14:57:23 -0800

    prepping for iron-session

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
 const login = async (req, res) => {
-  const credentials = req.body;
-
-  // console.log("credentials: ", credentials);
-
   try {
     const response = await axios.post(
       `https://admin.rbcommunity.org/auth/local`,
@@ -13,11 +9,7 @@ const login = async (req, res) => {
         password: req.body.password,
       }
     );
-    // console.log("RES: ", response);
-    // return res.response;
-    // res.status(201).end(JSON.stringify(response))
-    // res.statusCode = 200;
-    // res.end(JSON.stringify(response));
+
     res.status(response.status).json(response.data);
   } catch (error) {
     console.log("E: ", error);
